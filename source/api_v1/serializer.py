994ef948b2c77693bf90dc224b8b77c63a7ccda6
DinaraKA <d.karybekova@gmail.com>
2019-12-14 18:07:21 +0600

    Added comment add in Serializer

--- source/api_v1/serializer.py
+++ source/api_v1/serializer.py
@@ -8,9 +8,17 @@ class CommentSerializer(ModelSerializer):
     comment_author = serializers.CharField(read_only=True)
     comment_created_at = serializers.DateTimeField(read_only=True, format="%Y-%m-%d %H:%M:%S")
 
+
+    def create(self, request):
+        user = self.context['request'].user
+        photo = request['photo']
+        text = request['text']
+        comment = Comment.objects.create(comment_author=user, photo=photo, text=text)
+        return comment
+
     class Meta:
         model = Comment
-        fields = ['id', 'text', 'comment_author', 'comment_created_at']
+        fields = ['id', 'text', 'photo', 'comment_author', 'comment_created_at']
 
 
 class LikeSerializer(ModelSerializer):
