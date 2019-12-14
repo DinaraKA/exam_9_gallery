1e2d36fced2f2019a105d008fa0f6400a0e08c4e
DinaraKA <d.karybekova@gmail.com>
2019-12-14 14:27:35 +0600

    Installed widget_tweaks. Added form.html and photo creation.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views import IndexView, PhotoView
+from webapp.views import IndexView, PhotoView, AddPhoto
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('photo/<int:pk>/', PhotoView.as_view(), name='photo_detail')
+    path('photo/<int:pk>/', PhotoView.as_view(), name='photo_detail'),
+    path('photo/add/', AddPhoto.as_view(), name='add_photo')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
 from webapp.models import Photo
 
@@ -16,4 +18,14 @@ class PhotoView(DetailView):
     model = Photo
     template_name = 'detail.html'
     context_object_name = 'photo'
+
+
+class AddPhoto(CreateView):
+    model = Photo
+    template_name = 'create.html'
+    fields = ['photo', 'note', 'like', 'author']
+
+    def get_success_url(self):
+        return reverse('webapp:photo_detail', kwargs={'pk': self.object.pk})
+
 
